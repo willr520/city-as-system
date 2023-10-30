09af0b5a9cf44126e0ff09650535a009e1a578e2
willr520 <willr520@newschool.edu>
2023-10-30 16:16:46 -0400

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+import requests
+import json
+
+# URL for the CSV data
+url = "https://data.cityofnewyork.us/resource/p5f6-bkga.csv"
+
+# Fetch CSV data
+response = requests.get(url)
+
+if response.status_code == 200:
+    # Read CSV data into a DataFrame
+    df = pd.read_csv(url)
+    
+    # Convert DataFrame to JSON format
+    json_data = df.to_json(orient='records')
+    
+    # Save JSON data to a file
+    with open("nyc-data.json", "w") as json_file:
+        json.dump(json.loads(json_data), json_file, indent=4)
+    
+    print("Data has been successfully fetched and saved as 'nyc_data.json'")
+else:
+    print("Failed to fetch data. Status code:", response.status_code)
---
+++ script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([40.61756999, -73.9548453459], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
